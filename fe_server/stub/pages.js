3ff80d3c629f7a4a1b8b78895aac57afc49850c5
william chandra <williamchans57@gmail.com>
2023-09-16 21:52:19 +0700

    new-server

--- fe_server/stub/pages.js
+++ fe_server/stub/pages.js
@@ -14,12 +14,6 @@ const pages = [
         description: "Hima Persis - Sejarah",
         image: `${process.env.ENV_HOST_NAME}/himapersis.jpg`
     },
-    {
-        slug:'berita',
-        title: "Hima Persis - Berita",
-        description: "Hima Persis - Berita",
-        image: `${process.env.ENV_HOST_NAME}/himapersis.jpg`
-    }
     //please add more if you want, detext based on slug
 ]
 module.exports = { pages,defaultMeta }
